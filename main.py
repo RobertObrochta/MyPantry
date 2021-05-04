b8d9073054d29c397a42d5416628e2462a6cfda2
Robert Obrochta <obrochtarobert13@gmail.com>
2021-05-03 20:25:20 -0500

    Polished insertItem function

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 
-# this dictionary will eventually be stored in a database, but for the purposes of visualizing and getting set up, store in the dictionary
+# these dictionaries will eventually be stored in a database, but for the purposes of visualizing and getting set up, store in the dictionary
 saved_items = {}
-                        # macronutrients HAS to be a dictionary with the name of macro : amount of macro. Same with additional_nutrients
-# this function will take a serving_size of a food and reduce it down to its base form (1g, or 1oz). Therefore, will change all of the contents and scale them accordingly
-def insertFood(food, serving_size, macronutrients, additional_nutrients = None):
-    saved_items[food] = [macronutrients, serving_size, additional_nutrients]
+saved_meals = {}
+                  
+# this function will take a serving_size of a food and reduce it down to its base form (1). Therefore, will change all of the macro contents and scale them accordingly
+def insertItem(item, serving_size, macronutrients, additional_nutrients = None):
+    'creates a new ingredient item'
+    if type(macronutrients) != dict:
+        print("Ensure that the macronutrients are stored as a dictionary.")
+        return False
+    # converts the macronutrients to floats instead of strings
+    else:
+        ingredient_details = [1, macronutrients, additional_nutrients]
+        # scale serving_size to its base form of 1 serving
+        for key, amount in macronutrients.items():
+            if type(amount) == int or type(amount) == float:
+                base_value = amount / serving_size
+                macronutrients[key] = base_value
+                saved_items[item.lower()] = ingredient_details
+
+            elif "g" in amount:
+                g_index = amount.find("g")
+                # checks to see if it is a mg content or not
+                mg_index = amount[g_index - 1]
+                if mg_index.isdigit():
+                    base_value = float(amount[0:g_index]) / serving_size
+                    macronutrients[key] = base_value
+                else: 
+                    base_value = float(amount[0:g_index - 1]) / serving_size
+                    macronutrients[key] = base_value
+                saved_items[item.lower()] = ingredient_details
+
+        return True
+
+
+# another function will make a meal (of custom name)
+# Meals will be bundled together, separate from the normal ingredients
+
+# ISSUE: Have to provide a way to adjust the amount of ingredients (a.k.a. serving size)
+# ingredientList should be a dict, key is the ingredient and val is the number of servings of that ingredient
+def createMeal(mealName = "Custom Meal", ingredientList = []):
+    'compiles each ingredient and adds it to the database'
+    pass
+    
+
+
 
 food = "pizza"
-macro = {"protein": "15g", "carbohydrates": '12g'}
+macro = {"fat" : "2g","protein" : 15, "carbohydrates" : '12g'}
 serving_size = 1
 
 food2 = "burger"
-macro2 = {"protein": "20g", "carbohydrates": '1g'}
+macro2 = {"protein" : "20g", "carbohydrates" : '1g', "sodium" : "125mg"}
 serving_size2 = 2
 
-insertFood(food, serving_size, macro)
-insertFood(food2, serving_size2, macro2)
+insertItem(food, serving_size, macro)
+insertItem(food2, serving_size2, macro2)
+
+createMeal("cheese", ["burger", "pizza"])
+
 print(saved_items)
